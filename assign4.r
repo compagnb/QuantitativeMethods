3b280cbefa9c184105dcea3295fbea2f72dd508b
Barbara Compagnoni <compagnb@newschool.edu>
2015-11-05 09:27:17 -0500

    update on wasting 4

--- assign4.r
+++ assign4.r
@@ -1,3 +1,5 @@
+
+
 # Barbara Compagnoni
 # Assignment 4
 
@@ -20,21 +22,20 @@
 2.d. There was not a significant effect for LOW birthweight, t(136.94) = 1.7737, p = 0.07834 with no low birthweights and low birthweights only small difference.
 2.e. The t score is 1.7737. The p value is  0.07834. Since the p value is not less then .5 (95% test) the results are not significant.
 
-3.a. LOW (0 = Birth Weight >= 2500g, 1 = Birth Weight < 2500g, can be ordinal through frequency table) is Dependant, Smoke (yes/no, nominal) is Independent.
-3.b. t.test(LOW~SMOKE, lowbwt)
-3.c. data:  LOW by SMOKE
-      t = -2.1766, df = 141.69, p-value = 0.03117
+3.a. BWT (is birth weight in grams, interval/ratio) is Dependant, Smoke (yes/no, nominal) is Independent.
+3.b. t.test(BWT~SMOKE, lowbwt)
+3.c. data:  BWT by SMOKE
+      t = 2.7095, df = 170, p-value = 0.00743
       alternative hypothesis: true difference in means is not equal to 0
       95 percent confidence interval:
-      -0.29240371 -0.01405928
+      76.46677 486.95979
       sample estimates:
-      mean in group FALSE  mean in group TRUE
-      0.2521739           0.4054054
-3.d. There was a significant effect for Smoking during pregnancy, t(141.69) = -2.1766, p = 0.03117 with no 25% of low birthweights occuring when mother's don't smoke and 40% happening when mother's do smoke during pregnancy.
-3.e. The t score is -2.1766. The p value is  0.03117 Since the p value is less then .5 (95% test) the results are significant.
+      mean in group FALSE  mean in group TRUE 
+           3054.957            2773.243 
+3.d. There was a significant effect for Smoking during pregnancy, t(170) = 2.7095, p = 0.00743 with a mean of 3054.957 for women who dont smoke and a mean of 2773.243 for women who do. 
+3.e. The t score is 2.7095. The p value is  0.00743 Since the p value is less then .5 (95% test) the results are significant.
 
 4.b. Recode the independent variable into a nominal or ordinal variable.
-
 
 
 
